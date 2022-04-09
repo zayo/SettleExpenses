88e622223d7e1ff94c954159f1ae70caa7508c96
Stanislav Nedbálek <stanislav.nedbalek@avast.com>
2022-04-09 11:31:22 +0200

    Migrate to ViewPager2

--- app/src/main/java/com/nislav/settleexpenses/MainActivity.kt
+++ app/src/main/java/com/nislav/settleexpenses/MainActivity.kt
@@ -2,33 +2,34 @@ package com.nislav.settleexpenses
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nislav.settleexpenses.databinding.ActivityMainBinding
-import com.nislav.settleexpenses.ui.main.SectionsPagerAdapter
+import com.nislav.settleexpenses.ui.main.PagerAdapter
 
+/**
+ * Main activity, holds tabs.
+ */
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
+        with(binding) {
+            val adapter = PagerAdapter(this@MainActivity)
+            viewPager.adapter = adapter
+            TabLayoutMediator(tabs, viewPager) { tab, position ->
+                tab.text = adapter.getTitle(position)
+            }.attach()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            // TODO move to fragments.
+            fab.setOnClickListener { view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/nislav/settleexpenses/ui/main/PagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.nislav.settleexpenses.ui.main
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.nislav.settleexpenses.R
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class PagerAdapter(
+    private val fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+
+    /**
+     * Returns title for the corresponding Tab on [position].
+     */
+    fun getTitle(position: Int) = fragmentActivity.getString(TAB_TITLES[position])
+
+    override fun getItemCount(): Int = TAB_TITLES.size
+
+    override fun createFragment(position: Int): Fragment =
+        PlaceholderFragment.newInstance(position + 1)
+
+
+}
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_1_expenses,
+    R.string.tab_2_contacts
+)
