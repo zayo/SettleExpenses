84f6a16208316c35ae96758b1c5f8df99021a93d
zayo <stanislav.nedbalek@gmail.com>
2023-03-22 23:14:48 +0100

    Refactor ExpensesScreen

--- app/src/main/java/com/nislav/settleexpenses/ui/main/expenses/ExpensesFragment.kt
+++ app/src/main/java/com/nislav/settleexpenses/ui/main/expenses/ExpensesFragment.kt
@@ -3,25 +3,8 @@ package com.nislav.settleexpenses.ui.main.expenses
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.FabPosition
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.nislav.settleexpenses.R
 import com.nislav.settleexpenses.db.entities.Expense
 import com.nislav.settleexpenses.ui.add.expense.AddExpenseActivity
 import com.nislav.settleexpenses.ui.detail.expense.ExpenseDetailActivity
@@ -34,40 +17,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ExpensesFragment : Fragment() {
 
-    private val viewModel: ExpensesViewModel by viewModels()
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, state: Bundle?) =
         ComposeView(requireContext()).withThemedContent {
-            Scaffold(
-                floatingActionButton = {
-                    FloatingActionButton(onClick = ::addNewExpense) {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = stringResource(R.string.add_new_expense),
-                            tint = MaterialTheme.colors.onSecondary
-                        )
-                    }
-                },
-                floatingActionButtonPosition = FabPosition.End,
-                content = { padding ->
-                    val data = viewModel.expenses.collectAsState(emptyList())
-                    if (data.value.isEmpty()) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(padding),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = stringResource(R.string.empty_expenses))
-                        }
-                    } else {
-                        Expenses(
-                            modifier = Modifier.padding(padding),
-                            expenses = data.value,
-                            onClick = ::showExpenseDetail
-                        )
-                    }
-                }
+            ExpensesScreen(
+                addNewExpense = ::addNewExpense,
+                showExpenseDetail = ::showExpenseDetail
             )
         }
 
--- app/src/main/java/com/nislav/settleexpenses/ui/main/expenses/ExpensesScreen.kt
+++ app/src/main/java/com/nislav/settleexpenses/ui/main/expenses/ExpensesScreen.kt
@@ -1,6 +1,5 @@
 package com.nislav.settleexpenses.ui.main.expenses
 
-import android.content.res.Configuration
 import androidx.annotation.Size
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,27 +20,75 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.themeadapter.material.MdcTheme
-import com.nislav.settleexpenses.util.COLOR_DISABLED
 import com.nislav.settleexpenses.R
 import com.nislav.settleexpenses.db.entities.ContactWithState
 import com.nislav.settleexpenses.db.entities.Expense
 import com.nislav.settleexpenses.db.entities.ExpenseWithContacts
 import com.nislav.settleexpenses.domain.initials
 import com.nislav.settleexpenses.domain.searchableName
-import com.nislav.settleexpenses.util.getColor
 import com.nislav.settleexpenses.ui.Samples
+import com.nislav.settleexpenses.util.COLOR_DISABLED
+import com.nislav.settleexpenses.util.DayNightPreview
+import com.nislav.settleexpenses.util.getColor
+
+@Composable
+fun ExpensesScreen(
+    vm : ExpensesViewModel  = hiltViewModel(),
+    addNewExpense: () -> Unit,
+    showExpenseDetail: (Expense) -> Unit,
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = addNewExpense) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.add_new_expense),
+                    tint = MaterialTheme.colors.onSecondary
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        content = { padding ->
+            val data = vm.expenses.collectAsStateWithLifecycle()
+            if (data.value.isEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = stringResource(R.string.empty_expenses))
+                }
+            } else {
+                Expenses(
+                    modifier = Modifier.padding(padding),
+                    expenses = data.value,
+                    onClick = showExpenseDetail
+                )
+            }
+        }
+    )
+
+}
 
 @Composable
 fun Expenses(
@@ -143,12 +191,7 @@ fun ContactIcon(contact: ContactWithState) {
     }
 }
 
-@Preview(name = "Expenses light theme")
-@Preview(
-    name = "Expenses dark theme",
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
+@DayNightPreview
 @Composable
 fun Preview() {
     MdcTheme {
